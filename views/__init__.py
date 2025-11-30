bdc98e79a45df1633756bb0caaed5154cf3cc8c6
Berend Driessen <117527206+driessenberend@users.noreply.github.com>
2025-11-30 19:17:58 +0000

    init dom maken

--- views/__init__.py
+++ views/__init__.py
@@ -1,6 +1,13 @@
 # views/__init__.py
 """
 UI-laag met de verschillende tabbladen.
-"""
 
-from . import home, matches, companies, personas, subsidies, newsletters  # noqa: F401
+Submodules:
+- home
+- matches
+- companies
+- personas
+- subsidies
+- newsletters
+"""
+sletters  # noqa: F401
